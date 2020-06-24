639b21db6700579854a4359e92f545897476721a
Mayank Rustagi <marustagi@deloitte.com>
2020-06-25 01:05:15 +0530

    Edit Contacts & Filter Contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -4,13 +4,21 @@ import ContactContext from "../../context/contact/ContactContext";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
+  const { contacts, filtered } = contactContext;
 
-  const { contacts } = contactContext;
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
